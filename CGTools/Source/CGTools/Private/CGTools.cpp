28b1d8cb9da42503ab971baf51da8e397499a88d
SKY123C <56999224+SKY123C@users.noreply.github.com>
2023-09-01 11:00:49 +0800

    Add files via upload

---
+++ CGTools/Source/CGTools/Private/CGTools.cpp
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CGTools.h"
+
+#define LOCTEXT_NAMESPACE "FCGToolsModule"
+
+void FCGToolsModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	
+}
+
+void FCGToolsModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+	
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FCGToolsModule, CGTools)
---
+++ CGTools/Source/CGTools/Private/PyInterface/CGToolsBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PyInterface/CGToolsBPLibrary.h"
+#include "CGTools.h"
+
+UCGToolsBPLibrary::UCGToolsBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UCGToolsBPLibrary::CGToolsSampleFunction(float Param)
+{
+	return -1;
+}
+
